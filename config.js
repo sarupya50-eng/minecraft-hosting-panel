c7635071e15620ea5432b4d6aa07adf5ed95ff94
sarupya50-eng <sarupya50@gmail.com>
2026-03-23 23:56:34 +0530

    Create config.js file with Node.js configuration module

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: 'localhost', // your database host
+    user: 'root',     // your database user
+    password: '',     // your database password
+    database: 'minecraft_db' // your database name
+};
