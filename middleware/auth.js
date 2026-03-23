49b703c5c50a7c6237372c6cf37c28ca2f508c08
sarupya50-eng <sarupya50@gmail.com>
2026-03-23 23:55:20 +0530

    Create middleware/auth.js for JWT authentication

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.jwtSecret);
+    req.userId = decoded.userId;
+    req.username = decoded.username;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+module.exports = { verifyToken };
